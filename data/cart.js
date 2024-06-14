5b2e41e52dd5d248197eeda2b26505e16910bdd2
Reinhard Kent <reinhardkent.d@gmail.com>
2024-06-14 18:16:57 +0800

    Save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,12 @@
 export let cart =  JSON.parse(localStorage.getItem('cart')) || [{
   productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   quantity: 2,
+  deliveryOptionId: '1'
 },
 {
   productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
   quantity: 1,
+  deliveryOptionId: '2'
 }]; //JSON.parse(localStorage.getItem('cart')); 
 
 // if (!cart) {
@@ -49,7 +51,6 @@ function addedToCart (productId) {
 }
 
 
-
 export function addToCart (productId, quantitySelector) {
   let matchingItem;
   const quantity = Number(quantitySelector.value);
@@ -66,6 +67,7 @@ export function addToCart (productId, quantitySelector) {
     cart.push({
       productId,
       quantity,
+      deliveryOptionId: '1'
     });
   }
 
