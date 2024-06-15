474398636e6658e2fdf5ac559e76c5ece080fbef
Reinhard Kent <reinhardkent.d@gmail.com>
2024-06-15 11:25:38 +0800

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary () {
 
@@ -122,6 +123,8 @@ export function renderOrderSummary () {
         container.remove(); //remove from the page  
         calculateCartQuantity(document.querySelector(
           '.js-checkout-header-middle-section'), 'checkout');
+
+        renderPaymentSummary();
       }); 
     });
 
@@ -194,6 +197,7 @@ export function renderOrderSummary () {
         element.addEventListener('click', () => {
           updateDeliveryOption(productId, deliveryOptionId);
           renderOrderSummary(); //recursion
+          renderPaymentSummary();
         });
       });
     }
