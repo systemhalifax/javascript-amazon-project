d56b068ea0f7603bd87ec3dce400576e1a3b6cae
Reinhard Kent <reinhardkent.d@gmail.com>
2024-06-17 10:47:06 +0800

    Test removeFromCart from cartTest.js

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -1,4 +1,4 @@
-import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
+import { addToCart, cart, loadFromStorage, removeFromCart } from "../../data/cart.js";
 
 describe('test suite: addToCart', () => {
   beforeEach(() => {
@@ -47,5 +47,48 @@ describe('test suite: addToCart', () => {
       'cart', 
       '[{"productId":"e43638ce-6aa0-4b85-b27f-e1d07eb678c6","quantity":1,"deliveryOptionId":"1"}]'
     );
+  });
+});
+
+describe('test suite: removeFromCart', () => {
+  beforeEach(() => {
+    spyOn(localStorage, 'setItem');
+  });
+
+  it('removes a product from the cart', () => {
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1,
+        deliveryOptionId: '1'
+      }]);
+    });
+    loadFromStorage();
+
+    removeFromCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart.length).toEqual(0);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([]));
+  });
+
+  it('does nothing if product is not in the cart', () => {
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1,
+        deliveryOptionId: '1'
+      }]);
+    });
+    loadFromStorage();
+
+    removeFromCart('83d4ca15-0f35-48f5-b7a3-1ea210004f2e');
+    expect(cart.length).toEqual(1);
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 1,
+      deliveryOptionId: '1'
+    }]));
   });
 });
