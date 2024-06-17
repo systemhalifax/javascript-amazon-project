77d941ae31294f180e958885f4ab98cf2212ea36
Reinhard Kent <reinhardkent.d@gmail.com>
2024-06-17 09:14:11 +0800

    Checked if the name and the price are displayed correctly in orderSummaryTest.js

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -43,14 +43,24 @@ describe('test suite: rednerOrderSummary', () => {
       expect(
         document.querySelectorAll('.js-cart-item-container').length
       ).toEqual(2);
-
       expect(
         document.querySelector(`.js-product-quantity-${productId1}`).innerText
       ).toContain('Quantity: 2');
-
       expect(
         document.querySelector(`.js-product-quantity-${productId2}`).innerText
       ).toContain('Quantity: 1');
+      expect(
+        document.querySelector(`.js-product-name-${productId1}`).innerText
+      ).toEqual('Black and Gray Athletic Cotton Socks - 6 Pairs');
+      expect(
+        document.querySelector(`.js-product-name-${productId2}`).innerText
+      ).toEqual('Intermediate Size Basketball');
+      expect(
+        document.querySelector(`.js-product-price-${productId1}`).innerText
+      ).toContain('$');
+      expect(
+        document.querySelector(`.js-product-price-${productId2}`).innerText
+      ).toContain('$');
   });
 
   it('removes a product', () => {
@@ -58,15 +68,12 @@ describe('test suite: rednerOrderSummary', () => {
     expect(
       document.querySelectorAll('.js-cart-item-container').length
     ).toEqual(1);
-
     expect(
       document.querySelector(`.js-cart-item-container-${productId1}`)
     ).toEqual(null);
-
     expect(
       document.querySelector(`.js-cart-item-container-${productId2}`)
     ).not.toEqual(null);
-
     expect(cart.length).toEqual(1);
     expect(cart[0].productId).toEqual(productId2);
   });
