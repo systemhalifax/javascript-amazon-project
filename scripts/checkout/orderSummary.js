0c042404ab4b48dadb349f8b1f327ab388624266
Reinhard Kent <reinhardkent.d@gmail.com>
2024-06-17 12:54:20 +0800

    Test update delivery option in orderSummaryTest.js

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -79,10 +79,10 @@ export function renderOrderSummary () {
 
       html += `
 
-      <div class="delivery-option js-delivery-option" data-product-id="${matchingProduct.id}" data-delivery-option-id="${deliveryOption.id}">
+      <div class="delivery-option js-delivery-option js-delivery-option-${matchingProduct.id}-${deliveryOption.id}" data-product-id="${matchingProduct.id}" data-delivery-option-id="${deliveryOption.id}">
         <input type="radio"
           ${isChecked ? 'checked' : ''}
-          class="delivery-option-input"
+          class="delivery-option-input js-delivery-option-input-${matchingProduct.id}-${deliveryOption.id}"
           name="delivery-option-${matchingProduct.id}">
         <div>
           <div class="delivery-option-date">
