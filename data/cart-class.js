18a6122ee2a401add65031963b475832f64c6eb3
Reinhard Kent <reinhardkent.d@gmail.com>
2024-06-18 17:12:52 +0800

    add comment

--- data/cart-class.js
+++ data/cart-class.js
@@ -5,13 +5,13 @@ class Cart {
   #localStorageKey; // private property
   addedMessageTimeouts = {};
 
-  //runs the code 
+  //setup code 
   constructor(localStorageKey) {
     this.#localStorageKey = localStorageKey;
     this.#loadFromStorage();
   }
 
-  #loadFromStorage () {
+  #loadFromStorage () { //private method
     this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey)) || [{
       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       quantity: 2,
